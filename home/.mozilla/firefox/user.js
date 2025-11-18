57be5061e299443452461886b0602ba7555e61a6
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2025-11-18 04:25:01 -0800

    Disable a bunch of artificial intelligence preferences in firefox

--- home/.mozilla/firefox/user.js
+++ home/.mozilla/firefox/user.js
@@ -23,3 +23,15 @@ user_pref("extensions.VimFx.find_from_top_of_viewport", false);
 user_pref("accessibility.typeaheadfind", false);
 user_pref("accessibility.typeaheadfind.manual", false);
 user_pref("browser.urlbar.trimURLs", false);
+// AI junk
+user_pref("browser.ml.enable", false);
+user_pref("browser.ml.chat.sidebar", false);
+user_pref("browser.ml.chat.shortcuts", false);
+user_pref("browser.ml.chat.page", false);
+user_pref("browser.ml.chat.page.footerBadge", false);
+user_pref("browser.ml.chat.page.menuBadge", false);
+user_pref("browser.ml.chat.menu", false);
+user_pref("browser.ml.linkPreview.enabled", false);
+user_pref("extensions.ml.enabled", false);
+user_pref("browser.tabs.groups.smart.enabled", false);
+user_pref("browser.tabs.groups.smart.userEnable", false);
